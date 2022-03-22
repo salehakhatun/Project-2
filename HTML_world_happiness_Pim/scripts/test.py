dae3b73778e85e6ae7b4fc3ccf41ee5671ae480f
Pim-PDML <93693811+Pim-PDML@users.noreply.github.com>
2022-03-22 18:26:40 +1100

    Add files via upload

---
+++ HTML_world_happiness_Pim/scripts/test.py
@@ -0,0 +1,30 @@
+# 1. import dependencies
+import pandas as pd
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+#Created connection with local database SQL
+rds_connection_string = "postgres:2021@localhost:5432/Happiness_Ranking"
+engine = create_engine(f'postgresql://{rds_connection_string}')
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/Happiness_dataset")
+def Happiness_dataset():
+    data = pd.read_sql_query('select * from Happiness_score', con=engine)
+    return jsonify(data.to_json())
+    
+# 4. Define main behavior 
+if __name__ == "__main__":
+    app.run(debug=True)
